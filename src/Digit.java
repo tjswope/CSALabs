da00fecb41c8a646eb7d8a3f22780b3622ce6231
tjswope <tjswope@gmail.com>
2024-06-14 15:57:17 -0400

    updated digit and digitmatcher

--- src/Digit.java
+++ src/Digit.java
@@ -1,4 +1,56 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Digit {
+	private int label;
+    private int[][] matrix;
 
+    public Digit(int label, int[] pixels) {
+    	this.label = label;
+        if (pixels.length != 784) {
+            throw new IllegalArgumentException("Pixel array must contain exactly 784 elements.");
+        }
+        matrix = new int[28][28];
+        constructMatrix(pixels);
+    }
+
+    private void constructMatrix(int[] pixels) {
+    	this.label = -1;
+        for (int i = 0; i < 28; i++) {
+            for (int j = 0; j < 28; j++) {
+                matrix[i][j] = pixels[i * 28 + j];
+            }
+        }
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public int getLabel() {
+    	return label;
+    }
+    
+    public void printMatrix() {
+        for (int i = 0; i < 28; i++) {
+            for (int j = 0; j < 28; j++) {
+                System.out.printf("%3d ", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public BufferedImage toBufferedImage() {
+        BufferedImage image = new BufferedImage(28, 28, BufferedImage.TYPE_BYTE_GRAY);
+        for (int i = 0; i < 28; i++) {
+            for (int j = 0; j < 28; j++) {
+                int pixelValue = matrix[i][j];
+                int rgb = pixelValue | (pixelValue << 8) | (pixelValue << 16);
+                image.setRGB(j, i, rgb);
+            }
+        }
+        return image;
+    }
 }
--- src/DigitMatcher.java
+++ src/DigitMatcher.java
@@ -1,4 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class PatternMatcher {
+public class DigitMatcher {
+    private ArrayList<Digit> digits;
 
+    public DigitMatcher(String filePath) throws IOException {
+        digits = new ArrayList<>();
+        
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        String line;
+        // Skip the header line if present
+        br.readLine();
+        while ((line = br.readLine()) != null) {
+            String[] values = line.split(",");
+            int label = Integer.parseInt(values[0]);
+            int[] pixels = new int[784];
+            for (int i = 0; i < 784; i++) {
+                pixels[i] = Integer.parseInt(values[i + 1]);
+            }
+            digits.add(new Digit(label, pixels));
+        }
+        br.close();
+    }
+
+    public ArrayList<Digit> getDigits() {
+        return digits;
+    }
+
+    public void printDigits() {
+        for (Digit digit : digits) {
+            System.out.println("Label: " + digit.getLabel());
+            digit.printMatrix();
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+        	DigitMatcher digitCollection = new DigitMatcher("src\train.csv");
+            digitCollection.printDigits();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
